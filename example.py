e6d0c308531debe86f0314060619f01632cafabc
Drew Stephens <drew@dinomite.net>
2010-10-10 21:42:45 -0700

    Changed main.py to example and created lib directory

--- example.py
+++ example.py
@@ -14,11 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import user_agent
+from lib.user_agent import UserAgent
 
 def main():
     userAgentString = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-    browser = user_agent.UserAgent.factory(userAgentString).pretty()
+    browser = UserAgent.factory(userAgentString).pretty()
     print browser
 
 if __name__ == '__main__':
