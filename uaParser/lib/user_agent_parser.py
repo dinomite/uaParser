778bdd3594945fd02d3f20ed771ffa6504b019fc
Drew Stephens <drew@dinomite.net>
2010-11-15 15:20:33 -0800

    Move all of the regexes for user_agent_parser to YAML

--- uaParser/lib/user_agent_parser.py
+++ uaParser/lib/user_agent_parser.py
@@ -155,105 +155,21 @@ def GetFilters(user_agent_string, js_user_agent_string=None,
     return filters
 
 
+# Build the list of user agent parsers from YAML
 yamlFile = open('uaParser/resources/user_agent_parser.yaml')
 yaml = yaml.load(yamlFile)
 yamlFile.close()
 
-browser_slash_version_names = yaml['browser_slash_version_names']
-browser_slash_main_version_names = yaml['browser_slash_main_version_names']
+USER_AGENT_PARSERS = []
+for parser in yaml['user_agent_parsers']:
+    regex = parser['regex']
 
-_P = UserAgentParser
-USER_AGENT_PARSERS = (
-    #### SPECIAL CASES TOP ####
-    # must go before Opera
-    _P(r'^(Opera)/(\d+)\.(\d+) \(Nintendo Wii', family_replacement='Wii'),
-    # must go before Browser/major_version.minor_version - eg: Minefield/3.1a1pre
-    _P(r'(Namoroka|Shiretoko|Minefield)/(\d+)\.(\d+)\.(\d+(?:pre)?)',
-         'Firefox ($1)'),
-    _P(r'(Firefox)/(\d+)\.(\d+)([ab]\d+[a-z]*)',
-         'Firefox Beta'),
-    _P(r'(Firefox)-(?:\d+\.\d+)?/(\d+)\.(\d+)([ab]\d+[a-z]*)',
-         'Firefox Beta'),
-    _P(r'(Namoroka|Shiretoko|Minefield)/(\d+)\.(\d+)([ab]\d+[a-z]*)?',
-         'Firefox ($1)'),
-    _P(r'(MozillaDeveloperPreview)/(\d+)\.(\d+)([ab]\d+[a-z]*)?'),
-    _P(r'(SeaMonkey|Fennec|Camino)/(\d+)\.(\d+)([ab]?\d+[a-z]*)'),
-    # e.g.: Flock/2.0b2
-    _P(r'(Flock)/(\d+)\.(\d+)(b\d+?)'),
+    family_replacement = None
+    if 'family_replacement' in parser:
+        family_replacement = parser['family_replacement']
 
-    # e.g.: Fennec/0.9pre
-    _P(r'(Fennec)/(\d+)\.(\d+)(pre)'),
-    _P(r'(Navigator)/(\d+)\.(\d+)\.(\d+)', 'Netscape'),
-    _P(r'(Navigator)/(\d+)\.(\d+)([ab]\d+)', 'Netscape'),
-    _P(r'(Netscape6)/(\d+)\.(\d+)\.(\d+)', 'Netscape'),
-    _P(r'(MyIBrow)/(\d+)\.(\d+)', 'My Internet Browser'),
-    _P(r'(Firefox).*Tablet browser (\d+)\.(\d+)\.(\d+)', 'MicroB'),
-    # Opera will stop at 9.80 and hide the real version in the Version string.
-    # see: http://dev.opera.com/articles/view/opera-ua-string-changes/
-    _P(r'(Opera)/.+Opera Mobi.+Version/(\d+)\.(\d+)',
-            family_replacement='Opera Mobile'),
-    _P(r'(Opera)/9.80.*Version\/(\d+)\.(\d+)(?:\.(\d+))?'),
+    major_version_replacement = None
+    if 'major_version_replacement' in parser:
+        major_version_replacement = parser['major_version_replacement']
 
-    # Palm WebOS looks a lot like Safari.
-    _P('(webOS)/(\d+)\.(\d+)', 'Palm webOS'),
-
-    _P(r'(Firefox)/(\d+)\.(\d+)\.(\d+(?:pre)?) \(Swiftfox\)', 'Swiftfox'),
-    _P(r'(Firefox)/(\d+)\.(\d+)([ab]\d+[a-z]*)? \(Swiftfox\)', 'Swiftfox'),
-
-    # catches lower case konqueror
-    _P(r'(konqueror)/(\d+)\.(\d+)\.(\d+)', 'Konqueror'),
-
-    # Maemo
-
-    #### END SPECIAL CASES TOP ####
-
-    #### MAIN CASES - this catches > 50% of all browsers ####
-    # Browser/major_version.minor_version.beta_version
-    _P(r'(%s)/(\d+)\.(\d+)\.(\d+)' % browser_slash_version_names),
-    # Browser/major_version.minor_version
-    _P(r'(%s)/(\d+)\.(\d+)' % browser_slash_main_version_names),
-    # Browser major_version.minor_version.beta_version (space instead of slash)
-    _P(r'(iRider|Crazy Browser|SkipStone|iCab|Lunascape|Sleipnir|Maemo Browser) (\d+)\.(\d+)\.(\d+)'),
-    # Browser major_version.minor_version (space instead of slash)
-    _P(r'(iCab|Lunascape|Opera|Android) (\d+)\.(\d+)'),
-    _P(r'(IEMobile) (\d+)\.(\d+)', 'IE Mobile'),
-    # DO THIS AFTER THE EDGE CASES ABOVE!
-    _P(r'(Firefox)/(\d+)\.(\d+)\.(\d+)'),
-    _P(r'(Firefox)/(\d+)\.(\d+)(pre|[ab]\d+[a-z]*)?'),
-    #### END MAIN CASES ####
-
-    #### SPECIAL CASES ####
-    #_P(r''),
-    _P(r'(Obigo|OBIGO)[^\d]*(\d+)(?:.(\d+))?', 'Obigo'),
-    _P(r'(MAXTHON|Maxthon) (\d+)\.(\d+)', family_replacement='Maxthon'),
-    _P(r'(Maxthon|MyIE2|Uzbl|Shiira)', major_version_replacement='0'),
-    _P(r'(PLAYSTATION) (\d+)', family_replacement='PlayStation'),
-    _P(r'(PlayStation Portable)[^\d]+(\d+).(\d+)'),
-    _P(r'(BrowseX) \((\d+)\.(\d+)\.(\d+)'),
-    _P(r'(POLARIS)/(\d+)\.(\d+)', family_replacement='Polaris'),
-    _P(r'(BonEcho)/(\d+)\.(\d+)\.(\d+)', 'Bon Echo'),
-    _P(r'(iPhone) OS (\d+)_(\d+)(?:_(\d+))?'),
-    _P(r'(iPad).+ OS (\d+)_(\d+)(?:_(\d+))?'),
-    _P(r'(Avant)', major_version_replacement='1'),
-    _P(r'(Nokia)[EN]?(\d+)'),
-    _P(r'(Black[bB]erry).+Version\/(\d+)\.(\d+)\.(\d+)',
-            family_replacement='Blackberry'),
-    _P(r'(Black[bB]erry)\s?(\d+)',
-            family_replacement='Blackberry'),
-    _P(r'(OmniWeb)/v(\d+)\.(\d+)'),
-    _P(r'(Blazer)/(\d+)\.(\d+)', 'Palm Blazer'),
-    _P(r'(Pre)/(\d+)\.(\d+)', 'Palm Pre'),
-    _P(r'(Links) \((\d+)\.(\d+)'),
-    _P(r'(QtWeb) Internet Browser/(\d+)\.(\d+)'),
-    #_P(r'\(iPad;.+(Version)/(\d+)\.(\d+)(?:\.(\d+))?.*Safari/',
-    #     family_replacement='iPad'),
-    _P(r'(Version)/(\d+)\.(\d+)(?:\.(\d+))?.*Safari/',
-         family_replacement='Safari'),
-    _P(r'(OLPC)/Update(\d+)\.(\d+)'),
-    _P(r'(OLPC)/Update()\.(\d+)', major_version_replacement='0'),
-    _P(r'(SamsungSGHi560)', family_replacement='Samsung SGHi560'),
-    _P(r'^(SonyEricssonK800i)', family_replacement='Sony Ericsson K800i'),
-    _P(r'(Teleca Q7)'),
-    _P(r'(MSIE) (\d+)\.(\d+)', family_replacement='IE'),
-)
-# select family, major_version, minor_version, beta_version from user_agent where beta_version regexp '[a-zA-Z]' group by family, major_version, minor_version, beta_version;
+    USER_AGENT_PARSERS.append(UserAgentParser(regex, family_replacement, major_version_replacement))
