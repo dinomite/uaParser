f898c3062d428955f50879c300f892551af5ff47
Drew Stephens <drew@dinomite.net>
2010-11-18 15:43:58 -0800

    Make example.py work. Dear Drew, plz2test after coding :-( kthx

--- example.py
+++ example.py
@@ -17,9 +17,9 @@
 from uaParser.lib import user_agent_parser
 
 def main():
-    userAgentString = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
+    user_agent_string = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
 
-    family, major_version, minor_version, other_version = user_agent_parser.Parse(string)
+    family, major_version, minor_version, other_version = user_agent_parser.Parse(user_agent_string)
 
     print "Browser is " + family + " version " + major_version +"."+ minor_version +"."+ other_version
 
